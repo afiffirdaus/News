16ea9ee875fe7215a7f0927938c17de55e6d4164
afifirdaus <afifmuhdafif@gmail.com>
2020-07-30 01:29:11 +0800

    done 36

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -97,22 +97,17 @@ class CategoryTile extends StatelessWidget {
   }
 }
 
-
-class  BlogTile extends StatelessWidget {
-
+class BlogTile extends StatelessWidget {
   final String imageUrl, title, desc;
-  BlogTile({@required this.imageUrl,@required this.title,@required this.desc})
+  BlogTile(
+      {@required this.imageUrl, @required this.title, @required this.desc});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        children: <Widget>[
-          Image.network(imageUrl),
-          Text(title),
-          Text(desc)
-        ],)
-      
+        children: <Widget>[Image.network(imageUrl), Text(title), Text(desc)],
+      ),
     );
   }
 }
