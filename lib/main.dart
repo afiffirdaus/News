7275c9e0c583f8ad7c4364a4e22e2ee8bd8b7012
afifirdaus <afifmuhdafif@gmail.com>
2020-08-11 00:31:22 +0800

    name changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Malaysian News',
+      title: 'Berita Malaysia',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.white,
